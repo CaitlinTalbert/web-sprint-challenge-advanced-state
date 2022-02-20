5457613b817ca65e1f160a5b150b29328011dd28
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-02-20 10:16:30 -0600

    GOT CLOCKWISE TO WORK!!!

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,42 +1,83 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import * as actionCreators from '../state/action-creators';
-
+import React from "react";
+import { connect } from "react-redux";
+import { moveClockwise, moveCounterClockwise } from "../state/action-creators";
 
 export function Wheel(props) {
-const {moveClockwise, moveCounterClockwise} = props;
+  const { moveClockwise, moveCounterClockwise } = props;
 
-const handleClockwise = (e) => {
-  const {value} = e.target
-  moveClockwise(value);
-  console.log(props)
-}
+  const handleClockwise = (e) => {
+    const { value } = e.target;
+    moveClockwise(value);
+    console.log(props);
+  };
 
-const handleCounter = () => {
+  const handleCounter = () => {};
 
-}
-  
   return (
-    //either active or not active 
-    // ? action || 
+    //either active or not active
+    // ? action ||
     // interpolate each line
-    //props is wheel 
+    //props is wheel
     //{`${props.wheel === 0 ? "cog active" : "cog"}`} style={{ "--i": 0 }}>{props.wheel === 0 ? "B" : ""}
     <div id="wrapper">
       <div id="wheel">
-        <div className= {`${props.wheel === 0 ? "cog active" : "cog"}`} style={{ "--i": 0 }}>{props.wheel === 0 ? "B" : ""}</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        <div
+          className={`${props.wheel === 0 ? "cog active" : "cog"}`}
+          style={{ "--i": 0 }}
+        >
+          {props.wheel === 0 ? "B" : ""}
+        </div>
+        <div
+          className={`${props.wheel === 1 ? "cog active" : "cog"}`}
+          style={{ "--i": 1 }}
+        >
+          {props.wheel === 1 ? "B" : ""}
+        </div>
+        <div
+          className={`${props.wheel === 2 ? "cog active" : "cog"}`}
+          style={{ "--i": 2 }}
+        >
+          {props.wheel === 2 ? "B" : ""}
+        </div>
+        <div
+          className={`${props.wheel === 3 ? "cog active" : "cog"}`}
+          style={{ "--i": 3 }}
+        >
+          {props.wheel === 3 ? "B" : ""}
+        </div>
+        <div
+          className={`${props.wheel === 4 ? "cog active" : "cog"}`}
+          style={{ "--i": 4 }}
+        >
+          {props.wheel === 4 ? "B" : ""}
+        </div>
+        <div
+          className={`${props.wheel === 5 ? "cog active" : "cog"}`}
+          style={{ "--i": 5 }}
+        >
+          {props.wheel === 5 ? "B" : ""}
+        </div>
+        {/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" onClick={handleCounter}>Counter clockwise</button>
-        <button id="clockwiseBtn" onClick={handleClockwise}>Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={handleCounter}>
+          Counter clockwise
+        </button>
+        <button id="clockwiseBtn" onClick={handleClockwise}>
+          Clockwise
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default connect(st => st, actionCreators)(Wheel);
+const mapStateToProps = (state) => {
+  return {
+    wheel: state.wheel.counter,
+  };
+};
+
+export default connect(mapStateToProps, {
+  moveCounterClockwise,
+  moveClockwise,
+})(Wheel);
--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,70 +1,77 @@
 // ❗ You don't need to add extra reducers to achieve MVP
-import { combineReducers } from 'redux'
-import * as types from './action-types'; 
+import { combineReducers } from "redux";
+import * as types from "./action-types";
 
-const initialWheelState = 0
+const initialWheelState = { counter: 0 };
 function wheel(state = initialWheelState, action) {
   switch (action.type) {
-    case types.MOVE_CLOCKWISE: 
+    case types.MOVE_CLOCKWISE:
       return {
-        ...state, 
-        initialWheelState: initialWheelState + 1
+        ...state,
+        counter: state.counter + 1,
       };
-    case types.MOVE_COUNTERCLOCKWISE: 
+    case types.MOVE_COUNTERCLOCKWISE:
       return {
-        ...state, 
-        initialWheelState: initialWheelState - 1
+        ...state,
+        counter: state.counter - 1,
       };
+    default:
+      return state;
   }
-  return state
 }
 
-const initialQuizState = null
+const initialQuizState = null;
 function quiz(state = initialQuizState, action) {
   switch (action.type) {
-    case types.SET_QUIZ_INTO_STATE: 
+    case types.SET_QUIZ_INTO_STATE:
       if (action.payload) {
-        return {...action.payload}
+        return { ...action.payload };
       } else {
-        return initialQuizState
+        return initialQuizState;
       }
-      default:
-        return state
-    }
+    default:
+      return state;
+  }
 }
 
-const initialSelectedAnswerState = null
+const initialSelectedAnswerState = null;
 function selectedAnswer(state = initialSelectedAnswerState, action) {
   switch (action.type) {
-    case types.SET_SELECTED_ANSWER: 
-      return action.payload
+    case types.SET_SELECTED_ANSWER:
+      return action.payload;
   }
-  return state
+  return state;
 }
 
-const initialMessageState = ''
+const initialMessageState = "";
 function infoMessage(state = initialMessageState, action) {
   switch (action.type) {
-    case types.SET_INFO_MESSAGE: 
-      return action.payload
+    case types.SET_INFO_MESSAGE:
+      return action.payload;
   }
-  return state
+  return state;
 }
 
 const initialFormState = {
-  newQuestion: '',
-  newTrueAnswer: '',
-  newFalseAnswer: '',
-}
+  newQuestion: "",
+  newTrueAnswer: "",
+  newFalseAnswer: "",
+};
 
 function form(state = initialFormState, action) {
   switch (action.type) {
-    case types.INPUT_CHANGE: 
-      return {...state, ...action.payload}
-    case types.RESET_FORM: 
-      return {...state, initialFormState}
+    case types.INPUT_CHANGE:
+      return { ...state, ...action.payload };
+    case types.RESET_FORM:
+      return { ...state, initialFormState };
   }
-  return state
+  return state;
 }
 
-export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form })
+export default combineReducers({
+  wheel,
+  quiz,
+  selectedAnswer,
+  infoMessage,
+  form,
+});
